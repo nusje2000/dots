866b819e9ba97a90ece32039b6f1c6570613b8ae
Maarten Nusteling <maarten.nusteling@gmail.com>
2024-03-20 16:20:46 +0100

    Added git config

--- bin/setup_links.sh
+++ bin/setup_links.sh
@@ -5,3 +5,4 @@ PROJECT_ROOT=$(dirname $(dirname "$0"))
 link_file "$PROJECT_ROOT/bash/.bash_aliases" "$HOME/.bash_aliases"
 link_file "$PROJECT_ROOT/bash/.profile" "$HOME/.profile"
 link_file "$PROJECT_ROOT/nvim" "$HOME/.config/nvim"
+link_file "$PROJECT_ROOT/git/.gitconfig" "$HOME/.gitconfig"
