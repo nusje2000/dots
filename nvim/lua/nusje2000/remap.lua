75a94fb6e2df81b86ec2e1e82e6d69240a1b20dc
Maarten Nusteling <maarten.nusteling@gmail.com>
2024-04-02 09:33:23 +0200

    Added quit shortcut

--- nvim/lua/nusje2000/remap.lua
+++ nvim/lua/nusje2000/remap.lua
@@ -1,6 +1,7 @@
 vim.keymap.set('t', '<Esc>', "<C-\\><C-n>")
 vim.keymap.set('n', '<leader>m', function () vim.cmd('split') vim.cmd('terminal btop') end)
 
+vim.keymap.set('n', '<leader>q', vim.cmd.quit)
 vim.keymap.set('n', '<leader>t', vim.cmd.tabnew)
 vim.keymap.set('n', '<leader>T', vim.cmd.tabclose)
 vim.keymap.set('n', '<A-l>', vim.cmd.tabnext)
