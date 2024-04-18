c21ef4c31fa62b735b3b60231f7b0cb5dcd4ccd2
Maarten Nusteling <maarten.nusteling@gmail.com>
2024-04-19 00:32:19 +0200

    Added PHP setup

--- bin/update.sh
+++ bin/update.sh
@@ -5,7 +5,6 @@ source $BIN_DIR/functions.sh
 
 logo
 
-exit 0
 if is_wsl; then
     debug "Environment is using WSL"
 else
@@ -53,6 +52,38 @@ if ! command_exists rustup; then
     success "rustup has been installed"
 else
     success "rustup is already installed"
+fi
+
+if ! command_exists php; then
+    loading "Installing php..."
+
+    sudo add-apt-repository ppa:ondrej/php -y
+    sudo apt update
+
+    install_if_missing "php8.3"
+    install_if_missing "php8.3-cli"
+    install_if_missing "php8.3-bz2"
+    install_if_missing "php8.3-curl"
+    install_if_missing "php8.3-mbstring"
+    install_if_missing "php8.3-intl"
+
+    success "php has been installed"
+else
+    success "php is already installed"
+fi
+
+if ! command_exists composer; then
+    loading "Installing composer..."
+
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('sha384', 'composer-setup.php') === 'dac665fdc30fdd8ec78b38b9800061b4150413ff2e3b6f88543c636f7cd84f6db9189d43a81e5503cda447da73c7e5b6') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    sudo mv composer.phar /usr/local/bin/composer
+
+    success "composer has been installed"
+else
+    success "composer is already installed"
 fi
 
 
