c081db468ba60efb3ea3c98bbe0d3f85d280f733
Maarten Nusteling <maarten.nusteling@gmail.com>
2024-04-19 16:24:41 +0200

    Added ngrok installation

--- bin/update.sh
+++ bin/update.sh
@@ -118,6 +118,20 @@ else
     success "n is already installed"
 fi
 
+if ! command_exists ngrok
+then
+    loading "installing ngrok"
+
+    curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
+    echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
+    sudo apt update
+    sudo apt install ngrok
+
+    success "ngrok has been installed"
+else
+    success "ngrok is already installed"
+fi
+
 header "Install applications"
 
 if [ ! -d ~/.tmux/plugins/tpm ]; then
