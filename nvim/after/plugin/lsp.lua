3c87098ba4c2ae2bbd1fa8e81d334a12cb3eaf80
Maarten Nusteling <maarten.nusteling@gmail.com>
2024-05-08 20:28:01 +0200

    Used telescope for LSP references

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,22 +1,54 @@
 local lsp_zero = require('lsp-zero')
+local lspconfig = require('lspconfig')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+    -- see :help lsp-zero-keybindings
+    lsp_zero.default_keymaps({ buffer = bufnr })
+
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = bufnr })
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-	"tsserver",
-	"eslint",
-	"lua_ls",
-	"rust_analyzer",
-  },
-  handlers = {
-    lsp_zero.default_setup,
-  },
+    ensure_installed = {
+        "tsserver",
+        "eslint",
+        "lua_ls",
+        "rust_analyzer",
+        "phpactor"
+    },
+    handlers = {
+        lsp_zero.default_setup,
+    },
 })
 
 vim.keymap.set('n', '<C-Space>', vim.lsp.buf.code_action)
+
+lsp_zero.set_sign_icons({
+    error = '✘',
+    warn = '',
+    hint = '󰟶',
+    info = '»'
+})
+
+local diagnostic_prefixes = {
+  [vim.diagnostic.severity.ERROR] = '✘',
+  [vim.diagnostic.severity.WARN] = '',
+  [vim.diagnostic.severity.HINT] = '󰟶',
+  default = '»',
+}
+
+vim.diagnostic.config({
+    virtual_text = {
+        prefix = "",
+        spacing = 1,
+        format = function(diagnostic)
+            return string.format(
+                "%s %s",
+                diagnostic_prefixes[diagnostic.severity]
+                or diagnostic_prefixes.default,
+                diagnostic.message
+            )
+        end
+    },
+})
