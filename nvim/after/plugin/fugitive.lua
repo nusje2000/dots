f7770c243f638a0f78e1d5e5ecb60057f4d8cad5
Maarten Nusteling <maarten.nusteling@gmail.com>
2024-08-27 09:39:37 +0200

    Added git rebase shortcut (vim)

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,3 +1,10 @@
 vim.keymap.set('n', '<leader>gg', vim.cmd.Git, { })
 vim.keymap.set('n', '<leader>gp', ':G push', { })
 vim.keymap.set('n', '<leader>gP', ':G push --force-with-lease', { })
+
+vim.keymap.set('n', '<leader>gr', function()
+    vim.cmd(string.format(
+        ':G rebase -i HEAD~%d',
+        vim.fn.input("Commit count > ")
+    ))
+end)
