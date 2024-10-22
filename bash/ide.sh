c2b0635302dabea81efd671f2184d78841b4cc77
Maarten Nusteling <maarten.nusteling@gmail.com>
2024-10-22 18:31:09 +0200

    Added clone option to ide command

--- bash/ide.sh
+++ bash/ide.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
+PROJECTS_DIR=$HOME/projects
 PROJECT="${1:-.}"
-PROJECT_DIR=$(realpath "$HOME/projects/$PROJECT")
+
+if [[ "$PROJECT" == git@* ]]; then
+    read -p "$PROJECT appears to be a git project, would you like to clone this project? [Y/n] " -n 1 -r
+    echo
+
+    if [[ $REPLY =~ ^[Nn]$ ]]; then
+        exit 1
+    fi
+
+    cd $PROJECTS_DIR
+    git clone $PROJECT
+    PROJECT="$(echo "$PROJECT" | sed -e 's/.*\///' -e 's/\.git$//')"
+fi
+
+PROJECT_DIR=$(realpath "$PROJECTS_DIR/$PROJECT")
 
 echo "[INFO] Loading project $PROJECT from path $PROJECT_DIR"
 
